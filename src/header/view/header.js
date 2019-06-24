2464c02005752b5493cc1396795144b8f98bb2ef
xwvike <xwvike@gmail.com>
2019-06-24 19:23:29 +0800

    完善header组件

--- src/header/view/header.js
+++ src/header/view/header.js
@@ -7,11 +7,21 @@ import './header.css';
 class Header extends Component {
     constructor(props) {
         super(props);
-        this.state = {a: null, b: -1};
+        this.state = {
+            a: null, b: -1, listUrl: {
+                '/edit': '编辑主页',
+                '/changePassword': '更改密码',
+                '/privacy_and_security': '隐私和安全',
+                '/help': '帮助中心',
+                '/us': '关于我们',
+                '': '退出',
+            }
+        };
         this.changeA = this.changeA.bind(this);
         this.changeB = this.changeB.bind(this);
         this.backUp = this.backUp.bind(this);
         this.historyBack = this.historyBack.bind(this);
+
     }
 
     changeA() {
@@ -37,10 +47,11 @@ class Header extends Component {
     render() {
         let {name, UserNameNull, SendUserName, option, ChangeOptions, setListUrl} = this.props;
         let path = window.location.pathname;
+        console.log(path);
         let logo = <div className='header-home'>
             <div className='header-post'></div>
             <div className='header-logo' onClick={this.backUp}></div>
-            <div className='header-addfriend'></div>
+            <div classNpame='header-addfriend'></div>
         </div>;
         let search = <div className='search-box'>
             <Link to='/result'>
@@ -64,18 +75,27 @@ class Header extends Component {
                 </div>
             </Link>
         </div>;
-        let changeAvatar = <div className='header-changeAvatar'><span onClick={this.historyBack}></span>头像<p>保存</p></div>;
+        let changeAvatar = <div className='header-changeAvatar'><span onClick={this.historyBack}></span>头像<p>保存</p>
+        </div>;
         let dynamic = <div className='header-dynamic'>动态</div>;
+        let comment = <div onClick={this.historyBack} className='header-comment'>评论</div>;
         let aboutme = <div className='header-five'>
             <div onClick={() => ChangeOptions()}
                  className={option ? 'header-option' : 'heacder-option-close'}></div>
             <div className='header-aboutme'>{option ? '个人主页' : '选项'}</div>
             {option ? <div className='header-addfriend'></div> : <div></div>}
         </div>;
-        const tips = (text) => <div className='header-tips'><span onClick={this.historyBack}></span>{text}</div>;
+        const tips = (text) => <div className='header-tips'><span onClick={this.historyBack}> </span>{text}</div>;
         let header = <div className='header'>
-            {path==='/changeAvatar'?changeAvatar:path === '/' ? logo : path === '/search' ? search : path === '/result' ? search : path === '/dynamic' ? dynamic : path === '/aboutme' ? aboutme : path === '/edit' ? tips(setListUrl[0].text) : path === '/changePassword' ? tips(setListUrl[1].text) : path === '/privacy_and_security' ? tips(setListUrl[2].text) : path === '/help' ? tips(setListUrl[3].text) : tips(setListUrl[4].text)}
-        </div>;
+            {path.match(/\/comment/)!==null?
+                comment:path === '/changeAvatar' ?
+                changeAvatar : path === '/' ?
+                    logo : path === '/search' ?
+                        search : path === '/result' ?
+                            search : path === '/dynamic' ?
+                                dynamic : path === '/aboutme' ?
+                                    aboutme : tips(this.state.listUrl[Object.keys(this.state.listUrl).find((item) => item === path)])}
+         </div>;
         return (
             <div>
                 {header}
