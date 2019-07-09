cff1e3ee1ba94a75f23ee8777fb05b8b24a623ca
xwvike <xwvike@gmail.com>
2019-07-09 18:47:36 +0800

    更改配置地址

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config  = {
-    url:'http://localhost:3030/',
-    avatar:'http://localhost:3030/avatar?'
+    url:'http://47.106.155.229:3030/',
+    avatar:'http://47.106.155.229:3030/avatar?'
 };
 
 export default config;
