0ba8110b7aeff43bb10b479c51e3cf716126a0b8
xwvike <xwvike@gmail.com>
2019-07-09 18:39:08 +0800

    beta 1.0.0

--- src/post/view/post.js
+++ src/post/view/post.js
@@ -6,7 +6,7 @@ import {connect} from 'react-redux'
 import './post.css';
 
 
-const li = (data,loginUser,likeComment,postId) => {
+const li = (data, loginUser, likeComment, postId) => {
     // const{count,setCount} = useState(1);
     return <li className='post-comment-li'>
         <div>
@@ -16,17 +16,18 @@ const li = (data,loginUser,likeComment,postId) => {
             <div>
                 <Link to={`/user/${data.userId}`} className='userid'>{data.userId} </Link>
                 <span className='comment-text'>{data.text}</span>
-                <span  className={data.like.find((item)=>item===loginUser.userId)===loginUser.userId?'unlike-reply':'like-reply'}> </span>
+                <span
+                    className={data.like.find((item) => item === loginUser.userId) === loginUser.userId ? 'unlike-reply' : 'like-reply'}> </span>
             </div>
             {/*<span className='likeLength'>{data.like.length}次赞</span><span className='reply'>回复</span>*/}
         </div>
     </li>
 };
 
-const Post = (data,loginUser,likePost,collectionPost,likeComment) => {
-    let value = data.likeUser.find((item)=>item===loginUser.userId)===loginUser.userId?1:-1;
-    let list = data.comment.length<=0?'':data.comment.map((item)=>{
-        return li(item,loginUser,likeComment,data.postId)
+const Post = (data, loginUser, likePost, collectionPost, likeComment) => {
+    let value = data.likeUser.find((item) => item === loginUser.userId) === loginUser.userId ? 1 : -1;
+    let list = data.comment.length <= 0 ? '' : data.comment.map((item) => {
+        return li(item, loginUser, likeComment, data.postId)
     });
     return <div className='post'>
         <div className='post-userInfo'>
@@ -36,14 +37,17 @@ const Post = (data,loginUser,likePost,collectionPost,likeComment) => {
             <span className='userid'>{data.userId}</span>
         </div>
         <div className='post-image'>
-            <img src={config.url+'image/'+data.pictureUrl[0]}/>
+            <img src={config.url + 'image/' + data.pictureUrl[0]}/>
         </div>
         <div className='post-operating'>
             <div className='button'>
-                <span className={value===1?'like':'unlike'} onClick={()=>likePost(data._id,loginUser.userId)}> </span>
+                <span className={value === 1 ? 'like' : 'unlike'}
+                      onClick={() => likePost(data._id, loginUser.userId)}> </span>
                 <Link to={`/comment/${data._id}`}><span className='comment'> </span></Link>
                 <span className='share'> </span>
-                <span className={data.CollectionUser.find((item)=>item===loginUser.userId)===loginUser.userId?'collection':'unCollection'} onClick={()=>collectionPost(data._id,loginUser.userId)}> </span>
+                <span
+                    className={data.CollectionUser.find((item) => item === loginUser.userId) === loginUser.userId ? 'collection' : 'unCollection'}
+                    onClick={() => collectionPost(data._id, loginUser.userId)}> </span>
             </div>
             <span className='likeNum'>{data.likeUser.length} 次赞</span>
             <div className='postText'>
@@ -54,8 +58,8 @@ const Post = (data,loginUser,likePost,collectionPost,likeComment) => {
                     className='comment-num'>{`全部 ${data.comment.length} 条评论`}</span></Link>
                 <div className='comment'>
                     <ul>
-                        {list[list.length-1]}
-                        {list[list.length-2]}
+                        {list[list.length - 1]}
+                        {list[list.length - 2]}
                         {/*{li(data.comment[data.comment.length-1],loginUser,likeComment,data.postId)}*/}
                         {/*{li(data.comment[data.comment.length-2],loginUser,likeComment,data.postId)}*/}
                         {/*{data.comment.map((item) => {*/}
